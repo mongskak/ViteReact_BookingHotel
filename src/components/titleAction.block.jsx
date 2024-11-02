0f3e71928465ed997a461b84a9c225c8ee1c9c40
Muhamad Rijal Rifai <rifairijal97@gmail.com>
2024-11-03 02:26:33 +0700

    test

--- src/components/titleAction.block.jsx
+++ src/components/titleAction.block.jsx
@@ -1,21 +1,21 @@
 import React from 'react'
-import {Flex, Text} from '@chakra-ui/react'
+import { Flex, Text } from '@chakra-ui/react'
 
-const TitleAction = ({title, action} ) => {
+const TitleAction = ({ title, action }) => {
   return (
-    <Flex 
-    alignItems={"center"}
-    justifyContent={"space-between"}
+    <Flex
+      alignItems={"center"}
+      justifyContent={"space-between"}
     >
-        <Text
+      <Text
         fontSize={'20px'}
         fontWeight={'bold'}
-        >
-            {title}
-        </Text>
-        <Text>
-            {action}
-        </Text>
+      >
+        {title}
+      </Text>
+      <Text>
+        {action}
+      </Text>
     </Flex>
   )
 }
